ba5e1fda80cd047ebc2d24bbf6db9a48ca0ad22d
Rouben Tchakhmakhtchian <rouben.tchakhmakhtchian@utoronto.ca>
2020-04-18 19:33:53 -0400

    Backporting cross-platform changes from V3 to V4
    Signed-off-by: Rouben Tchakhmakhtchian <rouben.tchakhmakhtchian@utoronto.ca>

--- UploadToMegaV4.ps1
+++ UploadToMegaV4.ps1
@@ -1,7 +1,7 @@
 ﻿<#
 This script will take a file or folder path and upload it to mega, encode the link, and then put it into a persistent file.
 Author: Disk546
-Last modified 4/12/20
+Last modified 4/18/20
 
 In order for this to work you need to first install megaCMD. You can get it here https://mega.nz/cmd
 This has only been tested on Windows thus far. It may work on Linux but I haven't tried it.
@@ -23,15 +23,63 @@ If your drive is out of space, or close to it the upload will start but when you
 Sometimes the loop that shows the progress won't end and just loop forever. The file will be uploaded but the script won't produce an encoded link or the text file.
 #>
 
+#### Dependencies ####
+# 1. PowerShell
+# 2. MEGAcmd: mega-whoami (.bat), mega-login (.bat), mega-df (.bat), mega-transfers (.bat),
+#    mega-export (.bat), mega-put.
+
 #################################################################
+# Detect the OS and try to set the environment variables for MEGAcmd.
+if ($IsWindows) {
+    $MEGApath = "$env:LOCALAPPDATA\MEGAcmd"
+    $OS = "Windows"
+    $PathVarSeparator = ";"
+    $PathSeparator = "\"
+}
+elseif ($IsMacOS) {
+    $MEGApath = "/Applications/MEGAcmd.app/Contents/MacOS"
+    $OS = "macOS"
+    $PathVarSeparator = ":"
+    $PathSeparator = "/"
+}
+elseif ($isLinux) {
+    $MEGApath = "/usr/bin"
+    $OS = "Linux"
+    $PathVarSeparator = ":"
+    $PathSeparator = "/"
+}
+else {
+    Write-Error "Unknown OS! Bailing..."
+    Exit
+}
+
 #################################################################
-#Check the environment variable for MEGAcmd. This gives access to the mega bat files
-Write-Host "Setting Megacmd Path. If this hangs, exit and retry" -ForegroundColor Yellow
-$env:PATH += ";$env:LOCALAPPDATA\MEGAcmd"
+# Check if MEGAcmd is already installed and in the PATH
+# This gives access to the MEGAcmd executables and wrapper scripts.
+$deps = "mega-whoami","mega-login","mega-df","mega-transfers","mega-export","mega-put"
+foreach ($dep in $deps) {
+    Write-Host -NoNewline "Checking for $dep..."
+    if (Get-Command $dep -ErrorAction SilentlyContinue) { 
+        Write-Host "found!"
+    }
+    else {
+        Write-Host "not found! I'm going to try and fix this by setting PATH..."
+        Write-Host "$OS detected! Assuming MEGAcmd lives under $MEGApath."
+        Write-Host "Checking for MEGAcmd and setting paths. If this hangs, exit and retry." -ForegroundColor Yellow
+        if (Test-Path $MEGApath) {
+            $env:PATH += "$PathVarSeparator$MEGApath"
+        }
+        else {
+            Write-Error "MEGAcmd doesn't seem to exist under $MEGApath! Please install" +
+            "MEGAcmd and/or update this script accordingly."
+            Exit
+        }
+    }
+}
 
 #Test to see if MEGAcmd is running and if not start it
 $ProcessActive = Get-Process MEGAcmdServer -ErrorAction SilentlyContinue
-if($ProcessActive -eq $null)
+if($null -eq $ProcessActive)
 {
     Write-host "MegaCMD is not running. Starting MegaCMD" -ForegroundColor Magenta
     #MEGAcmdShell
@@ -42,23 +90,22 @@ else
 }
 #################################################################
 #This will test to see if a user is logged in and if not prompt them to log in
-$testLogin = mega-whoami.bat
-
+$testLogin = mega-whoami
 if ($testLogin -like '*Not logged in.*')
 {
     Write-Host "User not logged in, prompting for credentials" -ForegroundColor Yellow
     $creds = Get-Credential -Message "Please enter your Mega username and password" 
 
-    mega-login.bat $creds.UserName $creds.GetNetworkCredential().Password 
+    mega-login $creds.UserName $creds.GetNetworkCredential().Password 
 }
 #################################################################
 #Display who the current user is and set the email as a variable for later
-mega-whoami.bat 
-$UserEmailPre = mega-whoami.bat
+$UserEmailPre = mega-whoami
+Write-Host $UserEmailPre
 $userEmail = $UserEmailPre.Split(" ",3)[2]
 #################################################################
 #Display current free space
-mega-df.bat
+mega-df
 #################################################################
 #This step asks for the file/folder path of the thing(s) you are trying to upload and then gets rid of any quotations if they appear
 $FilePath = Read-Host "Enter the entire file path of the file OR folder you want to upload. Be sure to include the file type (if applicable). This is case sensitive"
@@ -74,30 +121,24 @@ Write-Host "If you are uploading a lot of files the script might hang for a litt
 mega-put -q $FilePath
 #################################################################
 #This section will show the current transfers and their upload progress. It repeats this until there are nothing being uploaded.
-#$isMegaEmpty = mega-transfers.bat --only-uploads
-$isMegaEmpty = $null
-do
-{
-    mega-transfers.bat --only-uploads
-    $isMegaEmpty = mega-transfers.bat --only-uploads
+Do {
+    $isMegaEmpty = mega-transfers --only-uploads
+    Write-Host $isMegaEmpty
 }
-while($isMegaEmpty -ne $null)
+While (![string]::IsNullOrEmpty($isMegaEmpty))
+
 #################################################################
 #Now that the upload is done this section will get the link, encode it to base64, and then export it to a notepad file for easy copy pasting. We will also set the clipboard to the encoded string
 #First need to get the file name. To do this we need to export the link(-a) and the -f flag to auto-accept the copyright notice
 $FileName = Split-Path -Path $Filepath -Leaf
-$ExportedLink = mega-export.bat -a -f  $FileName
+$ExportedLink = mega-export -a -f  $FileName
 $ShortLink = $ExportedLink.Split(":",2)[1]    
-
 #Next, we need to encode it.
 $sEncodedString=[Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($ShortLink))
-write-host $sEncodedString
-
+Write-Host $sEncodedString
 #Next, we are going to check and see if the destination folder exists and if not create it
-$FolderPath = "C:\Users\" + $env:USERNAME + "\Documents\EncodedMegaTxts"
-$FolderExists = Test-path $FolderPath
-
-if($FolderExists -eq $false)
+$FolderPath = $HOME + $PathSeparator + "Documents" + $PathSeparator + "EncodedMegaTxts"
+if (!(Test-Path $FolderPath))
 {
     Write-Host "Encoded text file location does not exist creating it at "  $FolderPath
     New-Item -Path $FolderPath -ItemType Directory 
@@ -106,8 +147,13 @@ if($FolderExists -eq $false)
 
 #Now that we have an encoded link, we put it in a notepad file (saved to same directory as the uploaded file) and open it
 $fileValue =  $UserEmail  + " || " +  $FilePath + " || " + $sEncodedString + " || " + $TotalSize
-Add-Content -Path $FolderPath\EncodedLinks.txt -Value $fileValue  
-start $FolderPath\EncodedLinks.txt
+Add-Content -Path ($FolderPath + $PathSeparator + "EncodedLinks.txt") -Value $fileValue  
+if ($IsWindows) {
+    start ($FolderPath + $PathSeparator + "EncodedLinks.txt")
+}
+else {
+    open ($FolderPath + $PathSeparator + "EncodedLinks.txt")
+}
 #################################################################
 #Ask for a user input before closing just in case there is an error that needs to be read before PowerShell closes
 Read-Host -Prompt "Press Enter to exit"
